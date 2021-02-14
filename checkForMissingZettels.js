f89565a1cd004c2beded100e421d5a69f5019278
Maggie Delano <maggie.delano@gmail.com>
2021-02-13 23:02:00 -0500

    Update to execute everything in one datalog query

--- checkForMissingZettels.js
+++ checkForMissingZettels.js
@@ -1,17 +1,4 @@
-// can probably combine the two functions in datalog itself...
-
-function getBlocksInCommon(tag1,tag2){
-  var blocksInCommon = window.roamAlphaAPI.q(`[:find ?blocks
-                                       :in $ [?tag1 ?tag2]
-                                       :where [?t1id :node/title ?tag1]
-                                              [?t2id :node/title ?tag2]
-                                              [?blocks :block/refs ?t1id]
-                                              [?blocks :block/refs ?t2id]]`,[tag1,tag2]);
-
-  return blocksInCommon.map((data, index) => {return data[0];});  
-}
-
-function getPagesNotOnIndexPage(container,blocksInCommon){
+function getPagesNotOnIndexPage(container,tag1,tag2){
     let ancestor = `[ 
   [(ancestor ?child ?parent)
    [?parent :block/children ?child]]
@@ -22,17 +9,21 @@ function getPagesNotOnIndexPage(container,blocksInCommon){
 
 var blocks = window.roamAlphaAPI.q(`
 [:find ?page_title
-  :in $ % ?container_title [?tagged_block ...]
-  :where 
+  :in $ % ?container_title ?tag1 ?tag2
+  :where
+    [?t1id :node/title ?tag1]
+    [?t2id :node/title ?tag2]
+    [?blocks :block/refs ?t1id]
+    [?blocks :block/refs ?t2id] 
     [?container_page :node/title ?container_title]
-    [?tagged_block :block/page ?page]
+    [?blocks :block/page ?page]
     [?page :node/title ?page_title]
     (not (ancestor ?container_block ?container_page)
          [?container_block :block/refs ?page])
-]`, ancestor, container, blocksInCommon);
+]`, ancestor, container, tag1,tag2);
 
   return blocks.map((data, index) => {return `[[${data[0]}]]`});
 }
 
-return getPagesNotOnIndexPage('[[Zettelkasten]] Index',getBlocksInCommon('Page Type', 'Zettel')).map((data, index) => {return `${data}`}).join('\n');
+return getPagesNotOnIndexPage('[[Zettelkasten]] Index','Zettel','Page Type').map((data, index) => {return `${data}`}).join('\n');
 
