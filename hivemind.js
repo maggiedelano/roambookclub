3caeb773bd1008d026b946a0170322f55e19d6b3
Maggie Delano <>
2022-09-17 17:37:38 -0400

    update with getRandomNote for random PQST

--- hivemind.js
+++ hivemind.js
@@ -83,3 +83,75 @@ window.roamjs.extension.smartblocks.registerCommand({
   text: "GETMISSINGQUESTIONS",
   handler: (context) => callbackQ,
 });
+
+
+function getRandomNote(pagePrefix){
+  let ancestor = `[ 
+  [(ancestor ?child ?parent)
+   [?parent :block/children ?child]]
+  [(ancestor ?child ?ancestor)
+   [?parent :block/children ?child]
+   (ancestor ?parent ?ancestor)]
+]`;
+
+var allPages = window.roamAlphaAPI.q(`
+[:find ?name
+  :in $ % ?tag
+  :where
+    [_ :node/title ?name]
+    [(clojure.string/starts-with? ^String ?name ?tag)]]
+]`, ancestor,pagePrefix);
+
+
+var randomPage = allPages[Math.floor(Math.random()*allPages.length)];
+  
+return randomPage.map((data, index) => {return `[[${data}]]`});
+}
+
+// add get random source
+function getRandomSource(){
+  return getRandomNote('S/');
+}
+
+const callbackRS = () => getRandomSource();
+
+window.roamjs.extension.smartblocks.registerCommand({
+  text: "GETRANDOMSOURCE",
+  handler: (context) => callbackRS,
+});
+
+// add get random question
+function getRandomQuestion(){
+  return getRandomNote('Q/');
+}
+
+const callbackRQ = () => getRandomQuestion();
+
+window.roamjs.extension.smartblocks.registerCommand({
+  text: "GETRANDOMQUESTION",
+  handler: (context) => callbackRQ,
+});
+
+// add get random proposition
+function getRandomProposition(){
+  return getRandomNote('P/');
+}
+
+const callbackRP = () => getRandomProposition();
+
+window.roamjs.extension.smartblocks.registerCommand({
+  text: "GETRANDOMPROPOSITION",
+  handler: (context) => callbackRP,
+});
+
+// add get random term
+function getRandomTerm(){
+  return getRandomNote('T/');
+}
+
+const callbackRT = () => getRandomTerm();
+
+window.roamjs.extension.smartblocks.registerCommand({
+  text: "GETRANDOMTERM",
+  handler: (context) => callbackRT,
+});
